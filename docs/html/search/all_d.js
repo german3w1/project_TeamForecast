d278171daf1c127e2971589df013a05fd7a5e72e
German <gerk2001@gmail.com>
2021-05-01 14:36:42 +0300

    output directory for doxygen fixed, readme synchronized

--- docs/html/search/all_d.js
+++ docs/html/search/all_d.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['paintevent_60',['paintEvent',['../class_expanding_weather_widget.html#a1a1805b04abe16824e381e5c9f21cb7c',1,'ExpandingWeatherWidget']]],
   ['pressure_5fvalue_61',['pressure_value',['../class_current_widget.html#a721767e07e42296445bd283f3c5794f6',1,'CurrentWidget']]],
-  ['project_5fteamforecast_62',['project_TeamForecast',['../md__c___users__german__documents__team_forecast__r_e_a_d_m_e.html',1,'']]]
+  ['project_5fteamforecast_62',['project_TeamForecast',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
